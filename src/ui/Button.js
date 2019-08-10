31e7f79ab371535a7c5a1b18e516b71728163d5d
Brian Buie <brianjamesbuie@gmail.com>
2019-08-10 14:09:08 -0500

    Button & strava icon

---
+++ src/ui/Button.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactComponent as StravaIcon } from '../static/strava-symbol.svg';
+
+function sizeMap(size) {
+  switch (size) {
+    case 'small':
+      return '0.7rem';
+    case 'large':
+      return '1.2rem';
+    default:
+      return size || '0.9rem';
+  }
+}
+
+function themeMap(theme) {
+  const link = { color: 'link', pad: 'none' };
+  switch (theme) {
+    case 'strava':
+      return { bkg: 'stravaOrange', color: 'white', DefaultIcon: StravaIcon };
+    case 'plain':
+      return { color: 'text', border: true };
+    case 'link':
+      return link;
+    default:
+      return link;
+  }
+}
+
+const StyledButton = styled.button`
+  border: ${({ border, color, theme }) => (border ? `1px solid ${theme[color || 'text']}` : 0)};
+  border-radius: 0.25em;
+  font-size: ${({ size }) => sizeMap(size)};
+  line-height: 1.2;
+  white-space: nowrap;
+  text-decoration: none;
+  padding: ${({ pad }) => (pad === 'none' ? 0 : '0.55em 1.1em')};
+  margin: 0.25em;
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+  flex-grow: ${({ grow }) => (grow ? '1' : '0')};
+  width: ${({ grow }) => (grow ? '100%' : 'auto')};
+  background: ${({ bkg, theme }) => (bkg ? theme[bkg] : 'none')};
+  color: ${({ color, theme }) => theme[color || 'text']};
+  transition: all ${({ theme }) => theme.timing}ms ease;
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+  path,
+  polygon {
+    fill: ${({ color, theme }) => theme[color || 'text']};
+  }
+  .icon {
+    height: 1em;
+    svg {
+      width: 1em;
+      height: 1em;
+    }
+  }
+  .text {
+    font-weight: bold;
+    text-transform: ${({ uppercase }) => (uppercase ? 'uppercase' : 'none')};
+  }
+  span:nth-child(2) {
+    margin-left: 0.5em;
+  }
+`;
+
+const Button = ({ label, Icon, theme, ...props }) => {
+  const { DefaultIcon, ...themeProps } = themeMap(theme);
+  const RenderIcon = Icon || DefaultIcon;
+  const mergedProps = { ...themeProps, ...props };
+  return (
+    <StyledButton {...mergedProps}>
+      {RenderIcon && (
+        <span className="icon">
+          <RenderIcon size={sizeMap(props.size)} />
+        </span>
+      )}
+      {label && <span className="text">{label}</span>}
+    </StyledButton>
+  );
+};
+
+const withLink = Button => ({ withLink, ...props }) =>
+  withLink ? (
+    <Link {...withLink}>
+      <Button {...props} />
+    </Link>
+  ) : (
+    <Button {...props} />
+  );
+
+export default withLink(Button);
---
+++ src/ui/Theme.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+const theme = {
+  bkg: '#282c34',
+  text: '#a6a6a6',
+  link: '#60dafb',
+  white: 'white',
+  stravaOrange: '#fc4c02',
+  timing: 200
+};
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 24px;
+    color: ${theme.text};
+    line-height: 1.5;
+    background: ${theme.bkg};
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+  }
+`;
+
+const CustomTheme = ({ children }) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </>
+);
+
+export default CustomTheme;
